59fc97b81c5fad4b1b4e332b9cb30c69c4085b1a
Peter Sonntag <peter@sonniger-tag.eu>
2025-07-08 21:21:28 +0200

    minor updates

--- src/app.rs
+++ src/app.rs
@@ -169,55 +169,55 @@ impl Component for App {
                 #[watch]
                 set_visible_child_name: model.current_ui_page,
 
-                add_child=&gtk::Box{
-                    set_orientation: gtk::Orientation::Vertical,
-                    set_vexpand: true,
-                    set_hexpand: true,
-
-                    #[local_ref]
-                    toast_overlay -> adw::ToastOverlay {
-                        #[local_ref]
-                        letter_grid -> gtk::Grid {
-                            set_orientation: gtk::Orientation::Horizontal,
-                            set_column_homogeneous: true,
-                            set_row_homogeneous: true,
-                            set_column_spacing: 0,
-                            set_row_spacing: 0,
-                            // set_halign: Align::Center,
-                            set_hexpand: true,
-                            //set_vexpand: true,
-                            set_row_spacing: 1,
-                            set_column_spacing: 1,
-                        }
-                    },
-                    gtk::Box {
+                add_child = &gtk::Box{
                         set_orientation: gtk::Orientation::Vertical,
                         set_vexpand: true,
                         set_hexpand: true,
 
                         #[local_ref]
-                        keyboard_row_1 -> gtk::Box {
-                            set_orientation: gtk::Orientation::Horizontal,
-                            set_hexpand: true,
-                            set_vexpand: true,
-                            // set_halign: Align::Center,
+                        toast_overlay -> adw::ToastOverlay {
+                            #[local_ref]
+                            letter_grid -> gtk::Grid {
+                                set_orientation: gtk::Orientation::Horizontal,
+                                set_column_homogeneous: true,
+                                set_row_homogeneous: true,
+                                set_column_spacing: 0,
+                                set_row_spacing: 0,
+                                // set_halign: Align::Center,
+                                set_hexpand: true,
+                                //set_vexpand: true,
+                                set_row_spacing: 1,
+                                set_column_spacing: 1,
+                            }
                         },
-                        #[local_ref]
-                        keyboard_row_2 -> gtk::Box {
-                            set_orientation: gtk::Orientation::Horizontal,
-                            set_hexpand: true,
+                        gtk::Box {
+                            set_orientation: gtk::Orientation::Vertical,
                             set_vexpand: true,
-                            // set_halign: Align::Center,
-                        },
-                        #[local_ref]
-                        keyboard_row_3 -> gtk::Box {
-                            set_orientation: gtk::Orientation::Horizontal,
                             set_hexpand: true,
-                            set_vexpand: true,
-                            // set_halign: Align::Center,
+
+                            #[local_ref]
+                            keyboard_row_1 -> gtk::Box {
+                                set_orientation: gtk::Orientation::Horizontal,
+                                set_hexpand: true,
+                                set_vexpand: true,
+                                // set_halign: Align::Center,
+                            },
+                            #[local_ref]
+                            keyboard_row_2 -> gtk::Box {
+                                set_orientation: gtk::Orientation::Horizontal,
+                                set_hexpand: true,
+                                set_vexpand: true,
+                                // set_halign: Align::Center,
+                            },
+                            #[local_ref]
+                            keyboard_row_3 -> gtk::Box {
+                                set_orientation: gtk::Orientation::Horizontal,
+                                set_hexpand: true,
+                                set_vexpand: true,
+                                // set_halign: Align::Center,
+                            }
                         }
-                    }
-                } -> { set_name: "game" },
+                    } -> { set_name: "game" },
 
                 add_child = &gtk::Box{
                         set_orientation: gtk::Orientation::Vertical,
